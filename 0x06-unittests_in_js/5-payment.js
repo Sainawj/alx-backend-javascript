735d194ee1f82aa279935ec5134c02f0c757bc75
Jonathan Saina <51122713+Sainawj@users.noreply.github.com>
2024-11-28 12:03:43 +0300

    Update 5-payment.js

--- 0x06-unittests_in_js/5-payment.js
+++ 0x06-unittests_in_js/5-payment.js
@@ -1,14 +1,14 @@
-// Import the 'Utils' module to access utility functions like calculateNumber
-const Utils = require("./utils").default;
+// Import the Utils module which contains the calculateNumber function
+const Utils = require('./utils');
 
-// Function to send a payment request by calculating the total amount including shipping
-function sendPaymentRequestToApi(totalAmount, totalShipping) {
-    // Call the calculateNumber function from Utils to compute the total (SUM of totalAmount and totalShipping)
-    const result = Utils.calculateNumber("SUM", totalAmount, totalShipping);
-
-    // Log the total amount to the console
-    console.log(`The total is: ${result}`);
-}
+// Define the sendPaymentRequestToApi function that calculates and logs the total cost
+const sendPaymentRequestToApi = (totalAmount, totalShipping) => {
+  // Calculate the total cost by calling calculateNumber with 'SUM' as the operation
+  const totalCost = Utils.calculateNumber('SUM', totalAmount, totalShipping);
+  
+  // Log the total cost to the console
+  console.log(`The total is: ${totalCost}`);
+};
 
 // Export the sendPaymentRequestToApi function for use in other modules
 module.exports = sendPaymentRequestToApi;
